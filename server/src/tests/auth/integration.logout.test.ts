05e6c427ca8d4100d07fb97cec6f20b0adb01222
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2026-05-29 10:27:59 +0300

    Modified logout route for validation of token. Added integration tests for it

--- server/src/tests/auth/integration.logout.test.ts
+++ server/src/tests/auth/integration.logout.test.ts
@@ -1,11 +1,30 @@
 import request from 'supertest';
 import app from '../../app';
+import createUser from '../helpers/createUser';
+import { prisma } from '../../lib/prisma';
+import { supabase } from '../../lib/supabase';
 
 describe("Logout route", () => {
+    let token: string;
+    let email = "integration.logout.test@admin.com";
+    let user_id: number;
+
+    beforeEach(async () => {
+        // Creating prock user with helper function
+        const user = await createUser(email);
+        user_id = user.id;
+
+        // Logging to created user to get token
+        const response = await request(app)
+            .post('/api/auth/login')
+            .send({ email: email, password: "123456" });
+        token = response.body.user.token;
+    });
 
     it('Logs out successfully', async () => {
         const response = await request(app)
             .post('/api/auth/logout')
+            .set('Cookie', `token=${token}`)
         expect(response.status).toBe(200);
         expect(response.body.message).toBe("Logged out successfully");
     });
@@ -13,7 +32,7 @@ describe("Logout route", () => {
     it('Clears token cookie', async () => {
         const response = await request(app)
             .post('/api/auth/logout')
-            .set('Cookie', ['token=fake-token']);
+            .set('Cookie', `token=${token}`)
 
         expect(response.status).toBe(200);
 
@@ -23,5 +42,30 @@ describe("Logout route", () => {
         expect(cookies).toBeDefined();
         expect(cookies).toContain('token=');
         expect(cookies).toContain('Expires=Thu, 01 Jan 1970');
+    });
+
+    it('Fails to logout if user is not logged in', async () => {
+        const response = await request(app)
+            .post('/api/auth/logout')
+        expect(response.status).toBe(401);
+        expect(response.body.error).toBe("Not logged in");
+    });
+
+    it("Fails to logout if token is invalid", async () => {
+        const response = await request(app)
+            .post('/api/auth/logout')
+            .set('Cookie', `token=invalid-token`)
+        expect(response.status).toBe(401);
+        expect(response.body.error).toBe("Invalid or expired session");
+    });
+
+    afterEach(async () => {
+        const user = await prisma.user.findUnique({ where: { id: user_id } });
+        
+        if (user?.supabase_id) {
+            await supabase.auth.admin.deleteUser(user.supabase_id);
+        }
+
+        await prisma.user.delete({ where: { id: user_id } });
     });
 });
