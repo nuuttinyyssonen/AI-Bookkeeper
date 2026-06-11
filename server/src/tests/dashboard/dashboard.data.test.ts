b40b37b3027fd09837406b824dc8ff438314eea0
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2026-06-11 09:42:36 +0300

    dashboard data tests

---
+++ server/src/tests/dashboard/dashboard.data.test.ts
@@ -0,0 +1,65 @@
+import request from "supertest";
+import app from "../../app";
+import createUser from "../helpers/createUser";
+import { prisma } from "../../lib/prisma";
+import { supabaseAdmin } from "../../lib/supabase";
+
+describe('dashboard data route', () => {
+    let token: string;
+    let email: string;
+    let user_id: string;
+
+    beforeAll(async () => {
+        email = `integration.dashboard.test${Date.now()}@admin.com`;
+        const user = await createUser(email);
+        user_id = user.id;
+        const response = await request(app)
+            .post('/api/auth/login')
+            .send({ email, password: '123456' });
+        token = response.body.user.token;
+    });
+
+    it('returns expenses, revenue and recent transactions', async () => {
+        const response = await request(app)
+            .get('/api/dashboard')
+            .set('Cookie', `token=${token}`)
+        
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            revenue: { _sum: { total_amount: null } },
+            expenses: { _sum: { total_amount: null } },
+            net_profit: 0,
+            recent_receipts: []
+        });
+    });
+
+    it('returns 401 if no token in request', async () => {
+        const response = await request(app)
+            .get('/api/dashboard')
+        
+        expect(response.status).toBe(401);
+        expect(response.body.message).toBe("Token is missing");
+    });
+
+    it('returns 401 if token is invalid', async () => {
+        const response = await request(app)
+            .get('/api/dashboard')
+            .set('Cookie', `token=$1239218390213i90wiqdkasdnl`)
+        
+        expect(response.status).toBe(401);
+        expect(response.body.message).toBe("Token is invalid or expired");
+    });
+
+    afterAll(async () => {
+        if(!user_id) return;  
+
+        const user = await prisma.user.findUnique({ where: { id: user_id } });
+        if (user?.supabase_id) {
+            await supabaseAdmin.auth.admin.deleteUser(user.supabase_id);
+        }
+
+        if (user_id) {
+            await prisma.user.delete({ where: { id: user_id } });
+        }
+    });
+});
