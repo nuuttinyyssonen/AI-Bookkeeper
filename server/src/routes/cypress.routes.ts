b241c0dab18c6ca78f8d86e70641cfd08f9a0f77
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2026-07-19 11:58:00 +0300

    Reviewd rate limiters, JWT cookies, file uploading (mime types, size validation) and AI prompts

--- server/src/routes/cypress.routes.ts
+++ server/src/routes/cypress.routes.ts
@@ -1,9 +1,10 @@
 import { deleteUserByEmail, grantSubscription } from "../controllers/cypress.controller";
 import { Router } from "express";
+import { rateLimiters } from "../utils/rateLimiter";
 
 const cypressRouter = Router();
 
-cypressRouter.delete('/', deleteUserByEmail);
-cypressRouter.post('/grant-subscription', grantSubscription);
+cypressRouter.delete('/', rateLimiters.read("cypress_e2e_delete"), deleteUserByEmail);
+cypressRouter.post('/grant-subscription', rateLimiters.read("cypress_e2e_post"), grantSubscription);
 
 export default cypressRouter;
